2e456a30a5af041feecc3f47f525c9f9f415d9d5
MatteoOliva <oliva.matteo98@outlook.com>
2024-02-20 19:07:40 +0100

    fix: img header da background css a tag img html. dato lo z index 1 a tutti gli elementi che hanno un over dato che venivano coperti dallo spazio in eccesso dei vari contenitori. cambiato il codice dei btn (non piu col padding)

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,66 +9,107 @@ export default {
 <template>
   <header>
     <div class="header-top">
-      <div class="header-logo"><img src="../../images/logo.png" alt=""></div>
+      <img class="background" src="../../images/index-banner-bg.png" alt="">
+      <div class="header-logo"><img src="../../images/logo.png" alt="" /></div>
       <div class="nav">
         <ul>
           <li>Home</li>
-          <li class="dropdown">Games<img src="../../images/svg/c-down-arrow.svg" alt="">
-          <div class="dropdown-content">
-            <a href="#">Games</a>
-            <a href="#">Games Details</a>
-            <a href="#">Match Details</a>
-          </div>
-        </li>
-          <li>Shop <span><img src="../../images/svg/c-down-arrow.svg" alt=""></span></li>
-          <li>Pages <span><img src="../../images/svg/c-down-arrow.svg" alt=""></span></li>
-          <li>Blog <span><img src="../../images/svg/c-down-arrow.svg" alt=""></span></li>
-          <li>Contact <span><img src="../../images/svg/c-down-arrow.svg" alt=""></span></li>
+          <li class="dropdown">
+            Games<img src="../../images/svg/c-down-arrow.svg" alt="" />
+            <div class="dropdown-content">
+              <a href="#">Games</a>
+              <a href="#">Games Details</a>
+              <a href="#">Match Details</a>
+            </div>
+          </li>
+          <li>
+            Shop
+            <span><img src="../../images/svg/c-down-arrow.svg" alt="" /></span>
+          </li>
+          <li>
+            Pages
+            <span><img src="../../images/svg/c-down-arrow.svg" alt="" /></span>
+          </li>
+          <li>
+            Blog
+            <span><img src="../../images/svg/c-down-arrow.svg" alt="" /></span>
+          </li>
+          <li>
+            Contact
+            <span><img src="../../images/svg/c-down-arrow.svg" alt="" /></span>
+          </li>
         </ul>
       </div>
       <div class="header-action">
-        <div class="chart"><img src="../../images/icon/cart-icon.png" alt="">
-        <span class="chart-count">03</span>
+        <div class="chart">
+          <img src="../../images/icon/cart-icon.png" alt="" />
+          <span class="chart-count">03</span>
         </div>
-        <div class="header-btn"> <span id="header-btn">LIVE STREAMING</span></div>
+        <div class="header-btn">
+          <span id="header-btn">LIVE STREAMING</span>
+        </div>
       </div>
-
     </div>
     <div class="header-bottom">
       <div class="header-bot-left">
-
         <div class="futureofesports">
-        <div><h4>Game with Live Streaming</h4></div>  
-        <div><h2>Future of Esports</h2></div>
-        <div><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt eligendi dolores vero adipisci quae dolorem natus</p></div>
-        <div class="headerbottom-btnsleft">
-          <div class="headerbottom-btnleft"> <span id="headerbottom-btnleft">EXPLORE GAMES <img src="../../images/svg/e-double-right-arrow.svg" alt=""></span>
+          <div><h4>Game with Live Streaming</h4></div>
+          <div><h2>Future of Esports</h2></div>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+              eligendi dolores vero adipisci quae dolorem natus
+            </p>
           </div>
-        <div class="play-icon"><div id="play-icon"><img src="../../images/icon/play-icon.png"></div>
+          <div class="headerbottom-btnsleft">
+            <div class="headerbottom-btnleft">
+              <span id="headerbottom-btnleft"
+                >EXPLORE GAMES
+                <img src="../../images/svg/e-double-right-arrow.svg" alt=""
+              /></span>
+            </div>
+            <div class="play-icon">
+              <div id="play-icon">
+                <img src="../../images/icon/play-icon.png"/>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="img-left">
+          <img src="../../images/game-controler.png" alt=""/>
         </div>
       </div>
-        
-      </div>
-
-      <div class="img-left"><img  src="../../images/game-controler.png" alt=""></div>
-
-    </div>
       <div class="header-bot-right">
+
         <div class="img-right">
-          <img src="../../images/index-banner-illus.png" alt="">
+          <img src="../../images/index-banner-illus.png" alt=""/>
+
+        </div>
       </div>
     </div>
-    </div>
+    
   </header>
 </template>
 
 <style lang="scss" scoped>
 @use "../styles/partials/variables.scss" as *;
-@use '../styles/partials/mixins.scss' as *;
+@use "../styles/partials/mixins.scss" as *;
 
 header {
-  background-image: url(../../images/index-banner-bg.png);
-  background-repeat: no-repeat;
+  // background-image: url(../../images/index-banner-bg.png);
+  // background-repeat: no-repeat;
+  width: 100%;
+  
+  position: relative;
+  .background {
+    position:absolute;
+    z-index: -1;
+    width: 100%;
+    height: 1700px;
+  
+  }
+
   .nav ul li {
     display: inline-block;
     color: $text-color;
@@ -77,42 +118,44 @@ header {
       cursor: pointer;
       color: $primary-color;
     }
-  img {
-    &:hover {
-      cursor: pointer;
+    img {
+      &:hover {
+        cursor: pointer;
+      }
     }
   }
+  .nav {
+    z-index: 1;
   }
+  
 
-  ul li:first-child{
+  ul li:first-child {
     color: $primary-color;
-    
   }
   .header-top {
     @include spaceAround;
-    padding-top: 40px
+    margin-top: 40px;
   }
   .header-action {
     @include center;
     .chart {
       position: relative;
       &:hover {
-      cursor: pointer;
-      
-    }
+        cursor: pointer;
+      }
     }
     .chart-count {
-    border-radius: 50%;
-    width: 20px;
-    height: 20px;
-    color: black;
-    background-color: $button-color;
-    padding: 2px;
-    position: absolute;
-    left: 50%;
-    bottom: 50%;
+      border-radius: 50%;
+      width: 20px;
+      height: 20px;
+      color: black;
+      background-color: $button-color;
+      padding: 2px;
+      position: absolute;
+      left: 50%;
+      bottom: 50%;
+    }
   }
-}
   .header-btn {
     margin-left: 40px;
     width: 200px;
@@ -120,139 +163,132 @@ header {
     border: 1px solid $button-color;
     border-radius: 20px;
     @include center;
-
   }
-    #header-btn {
-      border-radius: 20px;
-      color: $secondary-color;
-      background-color: $button-color;
-      padding: 12.7px 28px;
-      &:hover {
+  #header-btn {
+    width: 190px;
+    height: 40px;
+    border-radius: 20px;
+    color: $secondary-color;
+    background-color: $button-color;
+    text-align: center;
+    line-height: 40px;
+    z-index: 1;
+    &:hover {
       cursor: pointer;
       background-color: $primary-color;
-      
     }
-    }
-    .dropdown {
-  background:transparent;
-  border: none;
   }
-  &:hover .dropdown-content {
-    display: block;
+  .dropdown:hover .dropdown-content {
     display: flex;
-   flex-direction: column;  
-   left: 37%;
+    flex-direction: column;
+    left: 37%;
   }
+
   .dropdown-content {
-   position: absolute;
-   padding: 10px;
-   background-color: $secondary-color;
-   display: none;
-   a {
-    color: $text-color;
-    text-decoration: none;
-    &:hover {
-      color: $primary-color;
+    position: absolute;
+    padding: 10px;
+    background-color: $secondary-color;
+    display: none;
+    a {
+      color: $text-color;
+      text-decoration: none;
+      &:hover {
+        color: $primary-color;
+      }
     }
-   }
-  
-  
-  
-
-  
-}
-.header-bottom {
-  display: flex;
-  max-width: 100%;
-  margin-top: -40px;
-  
-}
-.header-bot-left {
-  width: 50%;
-  position: relative;
-  @include center;
-  margin-top: -220px;
-  
-}
-
-.header-bot-right {
-  width: 50%;
-  img {
-    max-width: 100%;
   }
-}
+  .header-bottom {
+    display: flex;
+    max-width: 100%;
+    margin-top: -40px;
+    height: 865px;
+  }
+  .header-bot-left {
+    width: 50%;
+    position: relative;
+    @include center;
+    margin-top: -220px;
+  }
 
-.futureofesports {
-  @include center;
-  flex-direction: column;
-  max-width: 50%;
-  margin-top: 100px;
-  margin-left: 200px;
-  
-
-  h4 {
-    font-size: 30px;
-    color: $primary-color;
-    margin-right: 90px;
+  .header-bot-right {
+    width: 50%;
+    img {
+      width: 100%
+    }
     
   }
-  h2 {
-    font-size: 75px;
-    color: $text-color;
-    margin-top: 10px;
-  }
-  p {
-    color: $underText-color;
-    margin-top: 10px;
-    font-size: 20px;
-  }
-}
-.headerbottom-btnsleft {
-  display: flex;
-  align-items: center;
-  width: 500px;
-  margin-top: 25px;
-}
 
-.headerbottom-btnleft { 
+  .futureofesports {
+    @include center;
+    flex-direction: column;
+    max-width: 50%;
+    margin-top: 100px;
+    margin-left: 200px;
+
+    h4 {
+      font-size: 30px;
+      color: $primary-color;
+      margin-right: 90px;
+    }
+    h2 {
+      font-size: 75px;
+      color: $text-color;
+      margin-top: 10px;
+    }
+    p {
+      color: $underText-color;
+      margin-top: 10px;
+      font-size: 20px;
+    }
+  }
+  .headerbottom-btnsleft {
+    display: flex;
+    align-items: center;
+    width: 500px;
+    margin-top: 25px;
+  }
+
+  .headerbottom-btnleft {
     width: 220px;
     height: 50px;
     border: 1px solid $button-color;
     border-radius: 20px;
     @include center;
     margin-left: 45px;
-   
-}
-#headerbottom-btnleft {
-      border-radius: 20px;
-      color: $secondary-color;
-      background-color: $button-color;
-      padding: 12.7px 28px;
-      &:hover {
+  }
+  #headerbottom-btnleft {
+    height: 40px;
+    line-height: 40px;
+    border-radius: 20px;
+    color: $secondary-color;
+    background-color: $button-color;
+    z-index: 1;
+    width: 200px;
+    text-align: center;
+    &:hover {
       cursor: pointer;
       background-color: $primary-color;
     }
-    }
-    .play-icon {
-      margin-left: 20px;
-      height: 60px;
-      width: 60px;
-      border-radius: 50%;
-      border: 1px solid $primary-color;
-      @include center;
-    }
-    #play-icon {
-      height: 40px;
-      width: 40px;
-      background-color: $text-color;
-      border-radius: 50%;
-      @include center;
-    }
-    .img-left {
-      position: absolute;
-      left: 7%;
-      top: 60%;
-      
-    }
-    }
+  }
+  .play-icon {
+    margin-left: 20px;
+    height: 60px;
+    width: 60px;
+    border-radius: 50%;
+    border: 1px solid $primary-color;
+    @include center;
+  }
+  #play-icon {
+    height: 40px;
+    width: 40px;
+    background-color: $text-color;
+    border-radius: 50%;
+    @include center;
+  }
+  .img-left {
+    position: absolute;
+    left: 7%;
+    top: 60%;
+  }
+}
 </style>
